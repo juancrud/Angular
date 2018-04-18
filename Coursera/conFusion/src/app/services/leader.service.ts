fd4b64e013202c35517eec2265ee959aa1594ca8
Juan Carlos Rudin <juan.rudin@proximitycr.com>
2018-04-18 17:39:00 -0600

    Simulating delays on service promises by using setTimeout.

--- Coursera/conFusion/src/app/services/leader.service.ts
+++ Coursera/conFusion/src/app/services/leader.service.ts
@@ -8,15 +8,21 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(LEADERS);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS), 2000);
+    });
   }
 
   getLeader(id: number): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter(x => x.id === id)[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter(x => x.id === id)[0]), 2000);
+    });
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter(x => x.featured)[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter(x => x.featured)[0]), 2000);
+    });
   }
 
 }
