c329dcd9bccaafee3b33bab01c05d59c38e5f48a
Juan Carlos Rudin <juan.rudin@proximitycr.com>
2018-04-26 20:01:01 -0600

    Adding Router.

--- EmptyApp/src/app/app.module.ts
+++ EmptyApp/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './modules/app-routing/app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { Test1Component } from './components/test1/test1.component';
+import { Test2Component } from './components/test2/test2.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Test1Component,
+    Test2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
