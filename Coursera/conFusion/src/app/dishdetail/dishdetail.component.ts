2693a0aed13d3ae019c68571c8a3e9889d80c31e
Juan Carlos Rudin <juan.rudin@proximitycr.com>
2018-05-03 11:51:07 -0600

    Adding form.

--- Coursera/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Coursera/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import 'rxjs/add/operator/switchMap';
 import { Dish } from '../shared/Dish';
 import { DishService } from '../services/dish.service'
@@ -17,7 +18,26 @@ export class DishdetailComponent implements OnInit {
   prev: number;
   next: number;
 
-  constructor(private dishservice: DishService, private location: Location, private route: ActivatedRoute) { }
+  addCommentForm: FormGroup;
+  formErrors = {
+    'name': '',
+    'comment': '' 
+  };
+  validationMessages = {
+    'name': {
+      'required': 'Name is required.',
+      'minlength': 'Name must be at least 2 characters long.',
+      'maxlength': 'Name cannot be more than 25 characters long.'
+    },
+    'comment': {
+      'required': 'Comment is required.',
+      'minlength': 'Comment must be at least 2 characters long.'
+    }
+  };
+
+  constructor(private dishservice: DishService, private location: Location, private route: ActivatedRoute, private fb: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit() {
     this.dishservice.getDishIds()
@@ -27,6 +47,34 @@ export class DishdetailComponent implements OnInit {
     this.route.params
       .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
       .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+  }
+
+  createForm() {
+    this.addCommentForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      comment: ['', [Validators.required, Validators.minLength(2) ]]
+    });
+
+    this.addCommentForm.valueChanges.subscribe(data => this.onValueChanged(data));
+    this.onValueChanged(); //reset form validation messages
+  }
+
+  addCommentFormSubmit() {
+    // this.feedback = this.feedbackForm.value;
+    // console.log(this.feedback);
+    // this.feedbackForm.reset({
+    //   firstname: '',
+    //   lastname: '',
+    //   telnum: '',
+    //   email: '',
+    //   agree: false,
+    //   contacttype: 'None',
+    //   message: ''
+    // });
+  }
+
+  onValueChanged(data?: any) {
+    
   }
 
   setPrevNext(dishId: number) {
