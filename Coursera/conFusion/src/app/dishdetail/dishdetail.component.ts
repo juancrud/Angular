2e98bed86ee88ff27ae6e8154f2c923d5346e538
Juan Carlos Rudin <juan.rudin@drivedominion.com>
2018-05-28 16:04:07 -0600

    Adding Animations.

--- Coursera/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Coursera/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,8 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import 'rxjs/add/operator/switchMap';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 import { Dish } from '../shared/Dish';
 import { DishService } from '../services/dish.service'
 
@@ -10,7 +12,20 @@ import { DishService } from '../services/dish.service'
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
   dish: Dish;
@@ -19,6 +34,7 @@ export class DishdetailComponent implements OnInit {
   prev: number;
   next: number;
   errorMessage: string;
+  visibility = 'shown';
 
   addCommentForm: FormGroup;
   formErrors = {
@@ -56,11 +72,15 @@ export class DishdetailComponent implements OnInit {
 
     // "+" converts the string into number
     this.route.params
-      .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-      .subscribe(dish => { 
-        this.dish = dish; 
-        this.dishcopy = dish; 
-        this.setPrevNext(dish.id); 
+      .switchMap((params: Params) => {
+        this.visibility = 'hidden';
+        return this.dishservice.getDish(+params['id']);
+      })
+      .subscribe(dish => {
+        this.dish = dish;
+        this.dishcopy = dish;
+        this.setPrevNext(dish.id);
+        this.visibility = 'shown';
       }, errMsg => this.errorMessage = <any>errMsg);
   }
 
